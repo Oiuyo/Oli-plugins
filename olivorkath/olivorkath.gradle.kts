0ebcf5f36453ae0c57fb306455e5fcae36bc16f6
Z <4021809+Soxs@users.noreply.github.com>
2022-01-14 17:41:14 +0000

    Uses PrayerUtils

--- olivorkath/olivorkath.gradle.kts
+++ olivorkath/olivorkath.gradle.kts
@@ -36,7 +36,8 @@ dependencies {
 
     compileOnly("com.openosrs:runelite-api:$openosrsVersion+")
     compileOnly("com.openosrs:runelite-client:$openosrsVersion+")
-    compileOnly(group = "com.openosrs.externals", name = "iutils", version = "4.7.5+");
+    compileOnly(group = "com.openosrs.externals", name = "iutils", version = "4.7.6+");
+    //compileOnly(project(":iutils"))
 
     compileOnly(Libraries.guice)
     compileOnly(Libraries.lombok)
